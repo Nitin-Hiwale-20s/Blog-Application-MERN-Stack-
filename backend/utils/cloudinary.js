be7e474ed9bc9450064bc1fa5fe17145cea2f7ea
Nitn Rajesh Hiwale <nitinhiwale67@gmail.com>
2025-11-19 21:22:56 +0530

    Cloudinary
    Reintroduce Cloudinary configuration and imports.

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,22 +1,3 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import dotenv from "dotenv"
-
-
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET,
-// });
-
-// export default cloudinary;
-
-
-
-
-
-
-
 
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
