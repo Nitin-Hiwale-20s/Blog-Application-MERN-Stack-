85ce29d4f7c48f1a5639262130c464d65bcf5c90
Nitn Rajesh Hiwale <nitinhiwale67@gmail.com>
2025-11-19 21:20:50 +0530

    isAuthenticated
    Removed commented-out code for the isAuthenticated middleware.

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,33 +1,4 @@
 
-// import jwt from "jsonwebtoken";
-
-// export const isAuthenticated = (req, res, next) => {
-
-//   const token =
-//     req.cookies?.token || req.headers["authorization"]?.split(" ")[1];
-
-//   if (!token) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "User not authenticated",
-//     });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-//     req.userId = decoded.userId;
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "User not authenticated",
-//     });
-//   }
-// };
-
-
-
-
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = (req, res, next) => {
